94b2c40b7744ec71399cb2792a09fb44cfc8ffdd
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-06 11:33:09 -0600

    Initial commit

---
+++ bin/persona_project.dart
@@ -0,0 +1,5 @@
+import 'package:persona_project/persona_project.dart' as persona_project;
+
+void main() {
+
+}
---
+++ test/persona_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:persona_project/persona_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
