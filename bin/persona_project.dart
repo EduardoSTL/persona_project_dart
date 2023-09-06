0a109a7bca345e46f95c10bf25a61b6078bd3082
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-06 12:19:57 -0600

    update

--- bin/persona_project.dart
+++ bin/persona_project.dart
@@ -1,5 +1,84 @@
-import 'package:persona_project/persona_project.dart' as persona_project;
-
 void main() {
+  final initProfesor = Profesor(sueldo: 100);
+  final initDirector = Director(sueldo: 500);
+  final initEmpleado = Empleado(sueldo: 300);
+  print('Profesor: $initProfesor');
+  print('Director: $initDirector');
+  print('Director: $initEmpleado');
+}
 
+interface class Persona{
+  String nombre;
+  final TipoPersona tipo;
+
+  Persona({
+    required this.nombre,
+    required this.tipo
+  });
+}
+
+abstract class Salario{
+  double salario;
+
+  Salario({
+    required this.salario
+  });
+}
+
+enum TipoPersona{director, empleado, profesor}
+
+class Profesor extends Persona implements Salario{
+  Profesor({required double sueldo})
+      : salario = sueldo, super(nombre: 'Osvaldo', tipo: TipoPersona.profesor);
+
+  @override
+  double salario;
+
+  @override
+  void Salario(double amount){
+    salario -= (amount * 0.5);
+  }
+
+  @override
+  String toString(){
+  return 'Salario: $salario, - Nombre: $nombre, Tipo: $tipo';
+}
+}
+
+class Director extends Persona{
+  Director({required double sueldo})
+      : salario = sueldo, super(nombre: 'Rodrigo', tipo: TipoPersona.director);
+
+  @override
+  double salario;
+
+  @override
+  void Salario(double amount){
+    salario -= (amount * 0.5);
+  }
+
+  @override
+  String toString(){
+  return 'Salario: $salario, - Nombre: $nombre, Tipo: $tipo';
+}
+  
+  
+}
+
+class Empleado extends Persona{
+Empleado({required double sueldo})
+      : salario = sueldo, super(nombre: 'Rodrigo', tipo: TipoPersona.empleado);
+
+  @override
+  double salario;
+
+  @override
+  void Salario(double amount){
+    salario -= (amount * 0.5);
+  }
+
+  @override
+  String toString(){
+  return 'Salario: $salario, - Nombre: $nombre, Tipo: $tipo';
+}  
 }
